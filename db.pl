cdf4cd2f4ca7a40b799121f8f9c2520a159a3731
Charles McCarthy <charles@studybundles.com>
2020-10-31 19:18:39 +0000

    Sorting Square Items

--- db.pl
+++ db.pl
@@ -1,13 +1,15 @@
 :- dynamic(square/3).
 :- dynamic square/3.
 
+board_size(9).
+board_length(3).
+
 square(3, 3, e).
 square(3, 2, e).
-square(3, 1, e).
 square(2, 3, e).
 square(2, 2, e).
 square(2, 1, e).
 square(1, 3, e).
 square(1, 2, e).
 square(1, 1, e).
-
+square(3, 1, e).
--- squares.pl
+++ squares.pl
@@ -20,8 +20,19 @@ move_player_to_square(Col, Row) :-
     assertz(square(Col, Row, 'P')),
     process_option.
 
+print_board([]).
+print_board([square(_, _, Item)|T]) :-
+%    indexOf([square(_, _, Item)|T], square(_, _, Item), A),
+%    X is X+1,
+%    (Row is 3, nl),
+%    write(A), nl,
+    write(Item), nl,
+    print_board(T).
+
 option(print) :-
-    square(_, _, _),
+    findall(square(Col, Row, Item), square(Col, Row, Item), Squares),
+    msort(Squares, Sorted),
+    print_board(Sorted),
     process_option.
 
 option(stop) :-
@@ -30,3 +41,16 @@ option(stop) :-
     listing(square),
     told,
     write('Done.'),nl.
+
+
+
+option(list) :- % Not needed - only used for testing
+    findall(square(Col, Row, Item), square(Col, Row, Item), Squares),
+    write(Squares), nl,
+    process_option.
+
+option(sort) :- % Not needed - only used for testing
+    findall(square(Col, Row, Item), square(Col, Row, Item), Squares),
+    msort(Squares, Sorted),
+    write(Sorted), nl,
+    process_option.
