3e5c1a64bde8ab78833b415965756523a4507c57
Charles McCarthy <charles@studybundles.com>
2020-11-01 11:03:14 +0000

    Clear Board & Fixed Board Generation Bugs

--- board.pl
+++ board.pl
@@ -1,18 +1,27 @@
 :- ensure_loaded('squares.pl').
 
 createBoard(N) :-
-    createBoard(N, N).
+    findall(square(R, Col, Item), square(R, Col, Item), Squares),
+    removeSquares(Squares),
+    persistBoard,
+    createBoard(N, N),
+    persistBoard,
+    write('Board has been created.'), nl,
+    !.
+
+createBoard(1, Col) :-
+    createCol(1, Col),
+    !.
 createBoard(Row, Col) :-
     createCol(Row, Col),
     NextRow is Row - 1,
-    NextRow > 0,
     createBoard(NextRow, Col).
 
 createCol(Row, 1) :-
-    assertz(square(Row, 1, e)).
+    assertz(square(Row, 1, e)),
+    !.
 createCol(CurrentRow, CurrentCol) :-
     NextCol is CurrentCol - 1,
-    NextCol > 0,
     assertz(square(CurrentRow, CurrentCol, e)),
     createCol(CurrentRow, NextCol).
 
@@ -20,9 +29,16 @@ persistBoard :-
     tell('squares.pl'),
     write(':- dynamic(square/3).'), nl,
     listing(square),
-    told,
-    write('Board has been created.'), nl.
+    told.
 
-%clearBoard :-
-%    retract(square(_, _, _)),
-%    write('Board has been destroyed.'), nl.
+clearBoard :-
+    findall(square(R, Col, Item), square(R, Col, Item), Squares),
+    removeSquares(Squares),
+    persistBoard,
+    write('Board has been destroyed.'), nl,
+    !.
+
+removeSquares([]).
+removeSquares([S|T]) :-
+    retract(S),
+    removeSquares(T).
--- squares.pl
+++ squares.pl
@@ -1,20 +1,28 @@
 :- dynamic(square/3).
 :- dynamic square/3.
 
-square(3, 2, e).
-square(3, 1, e).
-square(2, 3, e).
-square(2, 2, e).
-square(2, 1, e).
-square(1, 3, e).
-square(1, 2, e).
-square(1, 1, e).
+square(5, 5, e).
+square(5, 4, e).
+square(5, 3, e).
+square(5, 2, e).
+square(5, 1, e).
+square(4, 5, e).
+square(4, 4, e).
+square(4, 3, e).
+square(4, 2, e).
+square(4, 1, e).
+square(3, 5, e).
+square(3, 4, e).
 square(3, 3, e).
 square(3, 2, e).
 square(3, 1, e).
+square(2, 5, e).
+square(2, 4, e).
 square(2, 3, e).
 square(2, 2, e).
 square(2, 1, e).
+square(1, 5, e).
+square(1, 4, e).
 square(1, 3, e).
 square(1, 2, e).
 square(1, 1, e).
