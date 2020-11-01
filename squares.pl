816f4ab2ebffaa5f782f532564c02b0b921c6c09
Charles McCarthy <charles@studybundles.com>
2020-11-01 00:06:58 +0000

    List Adjacent Locations

--- squares.pl
+++ squares.pl
@@ -1,9 +1,11 @@
-:- ensure_loaded('db.pl').
+:- ensure_loaded('board.pl').
 :- ensure_loaded('directions.pl').
 
 start :-
     write('Welcome to the Squares game!.'), nl,
     write('Type "print." to view the board.'), nl,
+    write('Type "adj." to see adjacent squares.'), nl,
+    write('Type "move." to move in another direction.'), nl,
     write('Type "stop." to quit.'), nl,
     nl,
     place_player_in_center,
@@ -17,9 +19,6 @@ place_player_in_center() :-
     move_player_to_square(2, 2).
 
 move_player_to_square(Row, Col) :-
-write("Here"),   nl,
-write(Row),   nl,
-write(Col),   nl,
     retract(square(Row, Col, _)),
     assertz(square(Row, Col, 'P')).
 
@@ -51,8 +50,42 @@ print_board([square(_, _, Item)|T]) :-
 
 % Print board: Possibly loop over squares, keep a list of lists. Add square to sub-list by index (row = index)
 
-get_player_location(X, Y) :-
-    square(X, Y, 'P').
+%get_player_location(X, Y) :-
+%    square(X, Y, 'P').
+
+get_adjacent_locations([], [], []).
+get_adjacent_locations([H|T], L, [H|I]) :-
+    is_user_square(H),
+    get_adjacent_locations(T, L, I).
+
+get_adjacent_locations([H|T], [H|L], I) :-
+    get_adjacent_locations(T, L, I).
+
+is_user_square(square(_, _, Item)) :-
+    Item = 'P'.
+
+user_square(Row, Col) :-
+    square(Row, Col, 'P').
+
+
+list_adjacent_options([]).
+list_adjacent_options([square(R, C, _)|T]) :-
+    user_square(Row, Col),
+    (
+        R < Row, C = Col ->
+            format('You can travel North. (~w, ~w)', [R, C]), nl
+        ; R > Row, C = Col ->
+            format('You can travel South. (~w, ~w)', [R, C]), nl
+        ; R = Row, C < Col ->
+            format('You can travel West. (~w, ~w)', [R, C]), nl
+        ; R = Row, C > Col ->
+            format('You can travel East. (~w, ~w)', [R, C]), nl
+        ;
+            write('Received unknown argument.'), nl
+    ),
+%    format('You can travel ~w', []), nl,
+    list_adjacent_options(T).
+
 
 option(print) :-
     findall(square(Row, Col, Item), square(Row, Col, Item), Squares),
@@ -62,15 +95,13 @@ option(print) :-
     process_option.
 
 option(adj) :- % List squares that are adjacent to the player
-%    Player = get_player_location(X, Y),
-%    write(Player), nl,
-%    write(X), nl,
-%    write(Y), nl,
-    findall(square(Row, 2, Item), square(Row, 2, Item), Horizontal),
-    findall(square(2, Col, Item), square(2, Col, Item), Vertical),
-%    msort(Squares, Sorted),
-    write(Horizontal), nl,
-    write(Vertical), nl,
+    user_square(R, C),
+    findall(square(Row, C, Item), square(Row, C, Item), Horizontal),
+    get_adjacent_locations(Horizontal, HReduced, _),
+    findall(square(R, Col, Item), square(R, Col, Item), Vertical),
+    get_adjacent_locations(Vertical, VReduced, _),
+    list_adjacent_options(HReduced),
+    list_adjacent_options(VReduced),
     process_option.
 
 option(move) :-
